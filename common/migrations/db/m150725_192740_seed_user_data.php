d812443346e7d02d585ac57269ac40094399777c
hlitash <gena.litash@aurocraft.com>
2018-09-10 15:57:10 +0300

    added model temp

--- common/migrations/db/m150725_192740_seed_user_data.php
+++ common/migrations/db/m150725_192740_seed_user_data.php
@@ -5,7 +5,6 @@ use yii\db\Migration;
 
 class m150725_192740_seed_user_data extends Migration
 {
-
     public static $TABLE_NAME = '{{%user}}';
 
     /**
@@ -17,7 +16,7 @@ class m150725_192740_seed_user_data extends Migration
         $this->insert(self::$TABLE_NAME, [
             'id' => 5,
             'username' => 'root',
-            'email' => 'root@aurocraft.com',
+            'email' => 'root@codeasg.com',
             'password_hash' => Yii::$app->getSecurity()->generatePasswordHash('root'),
             'auth_key' => Yii::$app->getSecurity()->generateRandomString(),
             'access_token' => Yii::$app->getSecurity()->generateRandomString(40),
@@ -28,7 +27,7 @@ class m150725_192740_seed_user_data extends Migration
         $this->insert(self::$TABLE_NAME, [
             'id' => 6,
             'username' => 'admin',
-            'email' => 'admin@bkw-group.com.ua',
+            'email' => 'admin@codeasg.com.ua',
             'password_hash' => Yii::$app->getSecurity()->generatePasswordHash('admin'),
             'auth_key' => Yii::$app->getSecurity()->generateRandomString(),
             'access_token' => Yii::$app->getSecurity()->generateRandomString(40),
